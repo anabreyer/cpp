413fa8c000e77d3634b2e54ec39ec090098a4a06
Ana ADUAN BREYER <aaduan-b@made-f0Br8s6.clusters.42paris.fr>
2023-11-20 18:37:11 +0100

    starting cpp 06

---
+++ cpp06/ScalarConvertor.hpp
@@ -0,0 +1,45 @@
+#ifndef SCALARCONVERTER_HPP
+# define SCALARCONVERTER_HPP
+// #include "OverFlowException.hpp"
+#include <limits>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <cmath>
+#define CHAR 0
+#define INT 1
+#define FLOAT 2
+#define DOUBLE 3
+#define UNDEFINED 4
+
+class ScalarConverter
+{
+    private:
+        ScalarConverter();
+		static bool isInvalidNumber(std::string input);
+		static bool containsMultiple(std::string input, std::string set);
+		static void	toChar(std::string input);
+		static void	toInt(std::string input);
+		static void	toFloat(std::string input);
+		static void	toDouble(std::string input);
+		static void	putChar(long double input, bool is_int);
+		static void	putInt(long double input, bool is_int);
+		static void	putFloat(long double input, bool is_int);
+		static void	putDouble(long double input, bool is_int);
+		static void putValues(long double input, bool is_int);
+		static void	undefined(std::string input);
+		static int	detectType(std::string input);
+    public:
+        static char		character;
+		static int		integer;
+		static float	fpoint;
+		static double	dpoint;
+		//OCCF
+		// ~ScalarConverter();
+		// ScalarConverter(const ScalarConverter& copy);
+		// ScalarConverter& operator = (const ScalarConverter& copy);
+		//methods
+		static void	convert(std::string input);
+};
+
+#endif
